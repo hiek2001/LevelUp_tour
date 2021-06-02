cce803ba4102cf780961b1f9a90bc29995ee1e72
jhbae0119 <jhbae.dev@gmail.com>
2021-06-02 21:27:34 +0900

    Levelup_tour_setting

---
+++ levelUpTour/src/main/java/com/levelUp/tour/LevelUpTourApplication.java
@@ -0,0 +1,13 @@
+package com.levelUp.tour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevelUpTourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevelUpTourApplication.class, args);
+	}
+
+}
---
+++ levelUpTour/src/test/java/com/levelUp/tour/LevelUpTourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levelUp.tour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelUpTourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
